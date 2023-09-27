088c4f833d4f97ec496732cdc9a7a632f0a90d2e
Marcos Antonio Mendes Paes <marcosampaes@gmail.com>
2023-09-27 09:40:08 -0300

    Teste

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 from Repositorios.ProdutoRepo import ProdutoRepo
 
 app = FastAPI()
@@ -115,3 +116,7 @@ async def root(request: Request):
 @app.get('/trocas', response_class=HTMLResponse)
 async def root(request: Request):
     return templates.TemplateResponse("trocas.html", {"request": request,})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
